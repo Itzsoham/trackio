8989916672b4c9e3c2df19c33835e02613d8bcfb
kai <137367860+Itzsoham@users.noreply.github.com>
2024-12-23 17:46:09 +0530

    Backend Setup + Prisma

---
+++ server/src/index.ts
@@ -0,0 +1,34 @@
+import express, { Router } from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+// ROUTES IMPORT
+import projectRoutes from "./routes/proejctRoutes";
+
+// CONFIGURATIONS
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("This is the backend server for Trackio.");
+});
+
+// ROUTES USE
+app.use("/projects", projectRoutes);
+
+// SERVER
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
